35b3fe23d2a6eb3fd5b16c7e58f35537ee5e547e
Asher Kilner <912288@swansea.ac.uk>
2019-11-20 09:16:49 +0000

    commit before revert

--- Assets/arDesk/scripts/placementControler.cs
+++ Assets/arDesk/scripts/placementControler.cs
@@ -1,26 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 
 [RequireComponent(typeof(ARRaycastManager))]
 public class placementControler : MonoBehaviour
 {
 
-    [SerializeField]
     private GameObject placedPrefab;
-           
-    public GameObject PlacedPrefab
-    {
-        get
-        {
-            return placedPrefab;
-        }
-        set
-        {
-            placedPrefab = value;
-        }
-    }
+
+    [SerializeField]
+    private GameObject optionsmenu;
+
+    [SerializeField]
+    private Button square;
+
+    [SerializeField]
+    private Button circle;
+
+    [SerializeField]
+    private Button tree;
+
 
     private ARRaycastManager arRaycastManager;
 
@@ -28,6 +29,16 @@ public class placementControler : MonoBehaviour
     {
         arRaycastManager = GetComponent<ARRaycastManager>();
 
+        ChangePrefab("square");
+
+        square.onClick.AddListener(() => ChangePrefab("Tree"));
+        circle.onClick.AddListener(() => ChangePrefab("Square"));
+        tree.onClick.AddListener(() => ChangePrefab("Sphere"));
+    }
+
+    void ChangePrefab(string prefabName)
+    {
+        placedPrefab = Resources.Load<GameObject>($"Prefabs/{prefabName}");
     }
 
     private bool TryGetTouchPos(out Vector2 touchPos)
@@ -40,27 +51,61 @@ public class placementControler : MonoBehaviour
         touchPos = default;
         return false;
     }
-
-
-
     private void Start()
     {
 
     }
 
-
+    private bool newTouch;
+    private float touchTime;
+    private float longPress = 1;
+    private bool calledLong = false;
     // Update is called once per frame
     void Update()
     {
-        if(!TryGetTouchPos(out Vector2 touchPos))
+        if (placedPrefab == null)
         {
             return;
         }
-        if (arRaycastManager.Raycast(touchPos,hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+        if (Input.touchCount == 1)
         {
-            var hitPos = hits[0].pose;
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                newTouch = true;
+                //write here code to be done imidiately on touch
+            }
+            if (newTouch)
+            {
+                //write here code that is done whilst touching
+                touchTime += Time.deltaTime;
+                if (touchTime > longPress)
+                {
+                    //write here code that is called every frame after long press
+                    if (!calledLong)
+                    {
+                        //code to be called once after long press
+                        calledLong = true;
 
-            Instantiate(placedPrefab, hitPos.position, hitPos.rotation);
+                        if (!TryGetTouchPos(out Vector2 touchPos))
+                        {
+                            return;
+                        }
+                        if (arRaycastManager.Raycast(touchPos, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+                        {
+                            var hitPos = hits[0].pose;
+                            Instantiate(optionsmenu, hitPos.position, hitPos.rotation); 
+
+                        }
+                    }
+                }
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                touchTime = 0;
+                calledLong = false;
+                newTouch = false;
+            }
         }
     }
 
---
+++ Assets/arDesk/scripts/selectPlacedObjects.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class selectPlacedObjects : MonoBehaviour
+{
+    [SerializeField]
+    private PlacementObject[] placedObjects;
+
+    [SerializeField]
+    private Color activeColor = Color.red;
+
+    [SerializeField]
+    private Color inactiveColor = Color.grey;
+
+    [SerializeField]
+    private Camera arCamera;
+
+    private Vector2 touchPosition = default;
+
+
+    private void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPosition = touch.position;
+            if(touch.phase == TouchPhase.Began)
+            {
+                Ray ray = arCamera.ScreenPointToRay(touch.position);
+                RaycastHit hitObject;
+                if(Physics.Raycast(ray,out hitObject))
+                {
+                    PlacementObject placementObject = hitObject.transform.GetComponent<PlacementObject>();
+                    if(placementObject != null)
+                    {
+                        ChangeSelectedObject(placementObject);
+                    }
+                }
+            }
+        }
+    }
+
+    private void ChangeSelectedObject(PlacementObject placementObject)
+    {
+        foreach(PlacementObject current in placedObjects)
+        {
+            MeshRenderer meshRenderer = current.GetComponent<MeshRenderer>();
+            if(placementObject != current)
+            {
+                current.isSelected = false;
+                meshRenderer.material.color = inactiveColor;
+            }
+            else
+            {
+                current.isSelected = true;
+                meshRenderer.material.color = activeColor;
+            }
+        }
+    }
+}
