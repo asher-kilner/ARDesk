f2b4be9b65ae515772382d817bbf3c6ba3f436ef
Asher Kilner <912288@swansea.ac.uk>
2020-03-19 12:45:04 +0000

    push before database implementation

---
+++ Assets/arDesk/scripts/PlaceNoteController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceNoteController : MonoBehaviour
+{
+    List<StickyPad> openPads = new List<StickyPad>();
+    public TextMesh content;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount == 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                StickyPad n = new StickyPad(content);
+                openPads.Add(n);
+            }
+        }
+
+        //display all open pads
+    }
+}
---
+++ Assets/arDesk/scripts/StickyPad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isSelected;
+    public TextMesh body;
+    public StickyPad(TextMesh contents){
+        isSelected = false;
+        body = contents;
+    }
+    public bool Selected
+    {
+        get
+        {
+            return this.isSelected;
+        }
+        set
+        {
+            isSelected = value;
+        }
+    }
+}
